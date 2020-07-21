9111426e425f1372266858d54ddae113469a0b60
Michael Chadwick <mchadwick@ucsd.edu>
2020-07-21 09:58:13 -0700

    added basic test

---
+++ tests/test-clocker.rb
@@ -0,0 +1,18 @@
+﻿require 'clocker'
+
+c1 = Clocker.new
+
+duration1 = c1.clock do
+  3.times { print 'c1'; sleep(1) }
+end
+
+puts
+puts duration1
+
+c2 = Clocker.new(show_messages: true)
+
+duration2 = c2.clock do
+  3.times { print 'c2'; sleep(1) }
+end
+
+puts duration2
